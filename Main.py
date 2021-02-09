4d61ee8e111056c8f357864072df7ef5aeda0449
FruitD <bradtylerred@gmail.com>
2021-02-08 22:25:31 -0700

    Started Polymorphism

--- Main.py
+++ Main.py
@@ -1,9 +1,19 @@
+from OnePieceCharacters import Characters
 from Pirates import Pirate
 from Navy import Marine
 
 Luffy = Pirate('Straw Hats', 'Monkey D. Luffy', 19, 'Gum-Gum', '1,500,000,000', 'Mayumi Tanaka')
 Smoker = Marine('Captain', 'Smoker', 36, 'Plume-Plume', 'Mahito Ohba')
 Sanji = Pirate('Straw Hats', 'Sanji Vinsmoke', 21, None, '330,000,000', 'Hiroaki Hirata')
+
+GoodGuys = Characters
+BadGuys = Characters
+
+GoodGuys = [Luffy, Smoker, Sanji]
+
+for i in range(len(GoodGuys)):
+    print(GoodGuys[i].show_fruit())
+
 
 print(Luffy.show_bounty())
 print(Sanji.show_bounty())
